56b6e8031c30aa4099b1a1e01e4d95100693891e
jhoysbou <neeroll21@gmail.com>
2021-01-20 20:52:55 +0300

    inti

---
+++ src/main/java/com/jhoysbou/TBot/TBotApplication.java
@@ -0,0 +1,13 @@
+package com.jhoysbou.TBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhoysbou/TBot/TBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhoysbou.TBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
