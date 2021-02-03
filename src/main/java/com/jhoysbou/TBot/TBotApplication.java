0a3d43a659d268c55e09c55628af32872ae1433d
jhoysbou <neeroll21@gmail.com>
2021-02-03 23:50:37 +0300

    authentication implemented

--- src/main/java/com/jhoysbou/TBot/TBotApplication.java
+++ src/main/java/com/jhoysbou/TBot/TBotApplication.java
@@ -2,8 +2,10 @@ package com.jhoysbou.TBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:login.properties")
 public class TBotApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/jhoysbou/TBot/configs/TBotWebSecurityConfigurerAdapter.java
@@ -0,0 +1,51 @@
+package com.jhoysbou.TBot.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class TBotWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+    private final String login;
+    private final String password;
+
+
+    public TBotWebSecurityConfigurerAdapter(@Value("${tbot.web.login}") String login,
+                                            @Value("${tbot.web.password}") String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(login)
+                .password(passwordEncoder().encode(password))
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/v1/notification").permitAll()
+                .anyRequest().hasRole("ADMIN")
+                .and()
+                .httpBasic();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
