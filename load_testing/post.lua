aa02808443969b88ec5e26fac7fad8572a04674e
jhoysbou <neeroll21@gmail.com>
2021-03-05 01:35:03 +0300

    small changes

--- load_testing/post.lua
+++ load_testing/post.lua
@@ -1,3 +1,3 @@
 wrk.method = "POST"
-wrk.body   = '{"event_id":"d298hd","type":"message_new","group_id":"2324341","object":{"message":{"id":3412,"date":1231248324,"peer_id":137239419,"from_id":137239419,"text":"Задать вопрос администратору","payload":""},"client_info":{"button_actions":"dsff"}}}'
+wrk.body   = '{"event_id":"d298hd","type":"message_new","group_id":"2324341","object":{"message":{"id":3412,"date":1231248324,"peer_id":137239419,"from_id":137239419,"text":"Места работы выпускников 💼","payload":""},"client_info":{"button_actions":"dsff"}}}'
 wrk.headers["Content-Type"] = "application/json"
